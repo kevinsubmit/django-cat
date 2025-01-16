67e3a9349d9131b35059f05edbd7dc06a42b8f9f
austin li <645267294@qq.com>
2025-01-16 13:29:16 -0500

    cat django

---
+++ main_app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+# Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+      return self.name
+    def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+  class Meta:
+    ordering = ['-date']
+
---
+++ main_app/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Cat,Feeding,Toy
+from django.contrib.auth.models import User # add this line to list of imports
+
+# include User serializer
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Add a password field, make it write-only
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+    
+    def create(self, validated_data):
+      user = User.objects.create_user(
+          username=validated_data['username'],
+          email=validated_data['email'],
+          password=validated_data['password']  # Ensures the password is hashed correctly
+      )
+      
+      return user
+
+
+class ToySerializer(serializers.ModelSerializer):
+   class Meta:
+    model = Toy
+    fields = '__all__'
+
+
+class CatSerializer(serializers.ModelSerializer):
+    fed_for_today = serializers.SerializerMethodField()
+    toys = ToySerializer(many=True, read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  # Make the user field read-only
+    class Meta:
+        model = Cat
+        fields = '__all__'
+    def get_fed_for_today(self, obj):
+        return obj.fed_for_today()
+
+class FeedingSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Feeding
+    fields = '__all__'
+    read_only_fields = ('cat',)
+
